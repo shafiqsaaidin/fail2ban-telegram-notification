76d89172fffd2495db72b7aac11b702f531a1cfa
musha <mushagh@gmail.com>
2018-02-19 23:14:06 +0800

    added new file

---
+++ fail2ban-telegram.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Sends text messages using Telegram
+# to alert webmaster of banning.
+
+# Require one argument, one of the following
+# start
+# stop
+# ban
+# unban
+# Optional second argument: Ip for ban/unband
+
+
+# Display usage information
+function show_usage {
+  echo "Usage: $0 action <ip>"
+  echo "Where action start, stop, ban, unban"
+  echo "and IP is optional passed to ban, unban"
+  exit
+}
+
+
+# Send notification
+function send_msg {
+  apiToken=540667387:AAHnZNzGfJVfCa2ZNXx1hzBFVHdY4PAl4HU
+  chatId=73804219
+  url="https://api.telegram.org/bot$apiToken/sendMessage"
+
+  curl -s -X POST $url -d chat_id=$chatId -d text="$1"
+  exit
+}
+
+
+# Check for script arguments
+if [ $# -lt 1 ]
+then
+  show_usage
+fi
+
+
+# Take action depending on argument
+if [ "$1" = 'start' ]
+then
+  msg='Fail2ban+just+started.'
+  send_msg $msg
+elif [ "$1" = 'stop' ]
+then
+  msg='Fail2ban+just+stoped.'
+  send_msg $msg
+elif [ "$1" = 'ban' ]
+then
+  msg=$([ "$2" != '' ] && echo "Fail2ban+just+banned+$2" || echo 'Fail2ban+just+banned+an+ip.' )
+  send_msg $msg
+elif [ "$1" = 'unban' ]
+then
+  msg=$([ "$2" != '' ] && echo "Fail2ban+just+unbanned+$2" || echo "Fail2ban+just+unbanned+an+ip." )
+  send_msg $msg
+else
+  show_usage
+fi
